e6c8db1b59f784d3d282b732bc5305f530b27637
magicalsoup <amagicalsoup@gmail.com>
2023-01-02 22:18:07 -0500

    fix lint issues

--- components/Results.tsx
+++ components/Results.tsx
@@ -16,29 +16,29 @@ export default function Results({correctWordCount, timeTakenInSeconds, accuracy,
     return (
         <div className="flex flex-col justify-center items-center h-screen w-full bg-orange-100">
             <div className="flex flex-col w-3/5 max-w-[1284px] gap-y-16">
-                <div className="font-raleway text-2xl">
+                <div className="font-raleway text-4xl">
                     Your Results
                 </div>
                 <div className="flex justify-between w-full">
                     <div className="grid grid-rows-2 grid-cols-2 gap-x-12 gap-y-12 w-1/2"> 
                         <div className="flex flex-col">
-                            <div className="font-raleway text-2xl">wpm</div>
-                            <div className="font-robotomono text-4xl text-lime-900">{wordsPerMinute}</div>
+                            <div className="font-raleway text-3xl">wpm</div>
+                            <div className="font-robotomono text-5xl text-lime-900">{wordsPerMinute}</div>
                         </div>
                         <div className="flex flex-col">
-                            <div className="font-raleway text-2xl">time</div>
-                            <div className="font-robotomono text-4xl  text-lime-900">{timeTakenInSeconds}</div>
+                            <div className="font-raleway text-3xl">time</div>
+                            <div className="font-robotomono text-5xl text-lime-900">{timeTakenInSeconds}</div>
                         </div>
                         <div className="flex flex-col">
-                            <div className="font-raleway text-2xl">acc</div>
-                            <div className="font-robotomono text-4xl  text-lime-900">{accuracy}%</div>
+                            <div className="font-raleway text-3xl">acc</div>
+                            <div className="font-robotomono text-5xl text-lime-900">{accuracy}%</div>
                         </div>
                     </div>
                     <div className="flex flex-col w-1/2">
                         <p className="font-raleway text-xl">You just improved your memory on:</p>
                         <div className="pl-6 space-y-6">
                             {texts && texts.map(({title, text}, index) =>
-                                <li className="font-robotomono text-xl">
+                                <li className="font-robotomono text-xl" key={index}>
                                     {title}
                                 </li>
                             )}
